a7bfda3c6ed2f823a9b6f73fbfbcb233d2f8a936
Mukund-K91 <koladiyamukund58@gmail.com>
2023-08-24 06:50:39 -0700

    done

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -26,45 +26,69 @@ class dashboard extends StatelessWidget {
         backgroundColor: Color(0xff002233),
       ),
       drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              padding: EdgeInsets.all(0),
-                child: Container(
-              color: Color(0xff002233),
-            )),
-            ListTile(
-              leading: Icon(FontAwesomeIcons.home,color: Color(0xff002233)),
-              title: Text("HOME",),
-            ),
-            ListTile(
-              leading: Icon(FontAwesomeIcons.user,color: Color(0xff002233)),
-              title: Text("PROFILE"),
-            ),
-            ListTile(
-              leading: Icon(FontAwesomeIcons.gear,color: Color(0xff002233)),
-              title: Text("SETTINGS"),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: Text("Contact us",style: TextStyle(fontSize: 15),),
-            ),
-            ListTile(
-              leading: Icon(FontAwesomeIcons.phone,color: Color(0xff002233)),
-              title: Text("78XXXXXX77"),
-            ),
-            ListTile(
-              leading: Icon(FontAwesomeIcons.globe,color: Color(0xff002233)),
-              title: Text("ecollege.net"),
-            ),
-            ListTile(
-              leading: Icon(FontAwesomeIcons.location,color: Color(0xff002233)),
-              title: Text("Suart"),
-            ),
-            TextButton(onPressed: (){}, child: Text("Log Out!"))
-          ],
-        ),
+        child: Drawer_code(),
       ),
     );
   }
 }
+
+class Drawer_code extends StatelessWidget {
+  const Drawer_code({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        const UserAccountsDrawerHeader(
+          decoration: BoxDecoration(color: Color(0xff002233)),
+          margin: EdgeInsets.all(0),
+          accountName: Text("MUKUND"),
+          accountEmail: Text("support@ecollege.com"),
+          currentAccountPicture: CircleAvatar(
+            foregroundImage: AssetImage('assets/images/ex_img.png'),
+          ),
+        ),
+        ListTile(
+          leading: Icon(FontAwesomeIcons.home, color: Color(0xff002233)),
+          title: Text(
+            "HOME",
+          ),
+        ),
+        ListTile(
+          leading: Icon(FontAwesomeIcons.user, color: Color(0xff002233)),
+          title: Text("PROFILE"),
+        ),
+        ListTile(
+          leading: Icon(FontAwesomeIcons.gear, color: Color(0xff002233)),
+          title: Text("SETTINGS"),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: Text(
+            "Contact us",
+            style: TextStyle(fontSize: 15),
+          ),
+        ),
+        ListTile(
+          leading: Icon(FontAwesomeIcons.phone, color: Color(0xff002233)),
+          title: Text("78XXXXXX77"),
+        ),
+        ListTile(
+          leading: Icon(FontAwesomeIcons.globe, color: Color(0xff002233)),
+          title: Text("ecollege.net"),
+        ),
+        ListTile(
+          leading: Icon(FontAwesomeIcons.location, color: Color(0xff002233)),
+          title: Text("Suart"),
+        ),
+        TextButton(
+            onPressed: () {},
+            child: Text(
+              "Log Out !",
+              style: TextStyle(fontSize: 15),
+            ))
+      ],
+    );;
+  }
+}
+
