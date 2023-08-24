ea8f07ab0379fef815a3e9b6aae8aa10da00e666
Mukund-K91 <koladiyamukund58@gmail.com>
2023-08-24 03:59:20 -0700

    done

--- lib/login.dart
+++ lib/login.dart
@@ -1,10 +1,101 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class login extends StatelessWidget {
   const login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("assets/images/login_bg.png"))),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Let's Sign in",
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Welcome Back,\nYou've been missed..",
+              style: TextStyle(fontSize: 20, color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Form(
+                child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "SP ID",
+                      labelStyle: TextStyle(fontSize: 15),
+                      prefixIcon:
+                          Icon(Icons.person_rounded, color: Color(0xff002233)),
+                      border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  obscuringCharacter: '*',
+                  decoration: InputDecoration(
+                      labelText: "PASSWORD",
+                      labelStyle: TextStyle(fontSize: 15),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Color(0xff002233),
+                      ),
+                      suffixIcon: Icon(
+                        Icons.remove_red_eye,
+                        color: Color(0xff002233),
+                      ),
+                      border: OutlineInputBorder()),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {}, child: Text("forget Password?"))),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        backgroundColor: Color(0xff002233),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => login(),
+                            ));
+                      },
+                      child: Text(
+                        "LOGIN",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
+    ));
   }
 }
