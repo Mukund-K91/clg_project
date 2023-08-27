d0d3ac13c6c4258b82ac5569faa05a676ca132cf
Mukund-K91 <koladiyamukund58@gmail.com>
2023-08-27 13:11:13 +0500

    done

--- lib/student_add.dart
+++ lib/student_add.dart
@@ -1,6 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+enum GenderTypeEnum { Donwloadable, Deliverable }
 
 class add_student extends StatelessWidget {
   const add_student({super.key});
@@ -51,15 +55,192 @@ class add_student extends StatelessWidget {
   }
 }
 
-class student_add_form extends StatelessWidget {
-  const student_add_form({super.key});
+class student_add_form extends StatefulWidget {
+  student_add_form({super.key});
 
   @override
+  State<student_add_form> createState() => _student_add_formState();
+}
+
+class _student_add_formState extends State<student_add_form> {
+  TextEditingController _date = TextEditingController();
+  @override
+  String _selectedGender = 'Male';
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("ADD Students"),
+        title: Text(
+          "ADD Students",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xff002233),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "SP ID",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "First Name",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Last Name",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: RadioListTile<String>(
+                      title: Text('Female'),
+                      value: 'Female',
+                      groupValue: _selectedGender,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedGender = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: RadioListTile<String>(
+                      title: Text('Male'),
+                      value: 'Male',
+                      groupValue: _selectedGender,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedGender = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _date,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(FontAwesomeIcons.calendar),
+                      labelText: "DOB",
+                      border: OutlineInputBorder()
+                  ),
+                  onTap: () async {
+                    DateTime? pickdate = await showDatePicker(context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(200),
+                        lastDate:DateTime.now(),);
+
+                    if(pickdate!=null){
+                      setState(() {
+                        _date.text=DateF
+                      });
+                    }
+                  },
+                ),
+              ),
+              Text("Contact Details",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Last Name",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Last Name",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      // SingleChildScrollView(
+      //     child: Padding(
+      //   padding: const EdgeInsets.all(10),
+      //   child: Form(
+      //       child: Column(
+      //     children: [
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child:
+      //         TextFormField(
+      //           decoration: InputDecoration(
+      //             labelText: "SP ID",
+      //             border: OutlineInputBorder(),
+      //           ),
+      //         ),
+      //       ),
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child:
+      //         TextFormField(
+      //           decoration: InputDecoration(
+      //             labelText: "First Name",
+      //             border: OutlineInputBorder(),
+      //           ),
+      //         ),
+      //       ),
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: TextFormField(
+      //           decoration: InputDecoration(
+      //             labelText: "Last Name",
+      //             border: OutlineInputBorder(),
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         width: 200,
+      //         padding: EdgeInsets.all(8.0),
+      //         child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.center,
+      //           children: [
+      //             RadioListTile(value: null, groupValue: null, onChanged: null)
+      //           ],
+      //         ),
+      //       ),
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: TextFormField(
+      //           decoration: InputDecoration(
+      //             labelText: "Full Name",
+      //             border: OutlineInputBorder(),
+      //           ),
+      //         ),
+      //       ),
+      //     ],
+      //   )),
+      // )),
     );
   }
 }
