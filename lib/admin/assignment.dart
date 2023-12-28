749c7e9ea8456b0590e15ed35da65635bbd6e430
het <hetdani63@gmail.com>
2023-12-28 16:03:49 +0530

    DONE

---
+++ lib/admin/assignment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Assignment extends StatelessWidget {
+  const Assignment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
