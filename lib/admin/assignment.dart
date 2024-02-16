4cc0c2a62dc72126ce2d01230f8b8a01d2d3381f
omkheni <omkheni223@gmail.com>
2024-02-16 16:44:00 +0530

    CHECK

--- lib/admin/assignment.dart
+++ lib/admin/assignment.dart
@@ -9,10 +9,79 @@ class AssignmentPage extends StatefulWidget {
 }
 
 class _AssignmentPageState extends State<AssignmentPage> {
+  String selectedValue = 'Item 1';
+
+// bakinu pachhi karu !!!!!!!
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      body: Column(
+        children: [
+          Container(
+            height: 130,
+            decoration: BoxDecoration(
+              color: Color(0xff225779),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(35),
+                bottomRight: Radius.circular(35),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset('assets/images/arrow.png'),
+                Text(
+                  'Create Assignment',
+                  style: TextStyle(color: Colors.white, fontSize: 23),
+                ),
+                SizedBox(
+                  width: 40,
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: DropdownButton<String>(
+                  value: selectedValue,
+                  borderRadius: BorderRadius.circular(10),
+                  elevation: 10,
+                  items: [
+                    DropdownMenuItem<String>(
+                      value: 'Item 1',
+                      child: Text('Item 1'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Item 2',
+                      child: Text('Item 2'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Item 3',
+                      child: Text('Item 3'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Item 4',
+                      child: Text('Item 4'),
+                    ),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      selectedValue = value!;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
