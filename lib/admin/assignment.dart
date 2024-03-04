9ffb0288527a2b26abd4b2e96dab046761ff9d0f
omkheni <omkheni223@gmail.com>
2024-03-04 18:07:44 +0530

    Assignment Done

--- lib/admin/assignment.dart
+++ lib/admin/assignment.dart
@@ -1,6 +1,7 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,6 +38,57 @@ var selectItem = carType.first;
 
 class _AssignmentPageState extends State<AssignmentPage> {
   bool isSwitched = false;
+
+  // TimeOfDay selectedTime = TimeOfDay.now();
+  //
+  // Future<void> _selectTime(BuildContext context) async {
+  //   final TimeOfDay? picked = await showTimePicker(
+  //     context: context,
+  //     initialTime: selectedTime,
+  //   );
+  //
+  //   if (picked != null && picked != selectedTime) {
+  //     setState(() {
+  //       selectedTime = picked;
+  //     });
+  //   }
+  // }
+
+  final formkey = GlobalKey<FormState>();
+  final toDateControler = TextEditingController();
+  final fromDateControler = TextEditingController();
+  Color formBorderColor = Colors.grey;
+
+  int calculateDaysDifference(String fromdate, String todate) {
+    DateFormat format = DateFormat("dd/MM/yyyy");
+    DateTime fromDate = format.parse(fromdate);
+    DateTime toDate = format.parse(todate);
+    Duration difference = toDate.difference(fromDate);
+    return difference.inDays;
+  }
+
+  TextEditingController _timeController = TextEditingController();
+  TimeOfDay selectedTime = TimeOfDay.now();
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+
+    if (picked != null && picked != selectedTime) {
+      setState(() {
+        selectedTime = picked;
+        _timeController.text = selectedTime.format(context);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _timeController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -210,26 +262,90 @@ class _AssignmentPageState extends State<AssignmentPage> {
                 Expanded(
                   child: Padding(
                     padding: const EdgeInsets.only(right: 10, left: 20),
+                    // child: TextFormField(
+                    //   decoration: InputDecoration(
+                    //     labelText: 'Due Date',
+                    //     border: OutlineInputBorder(
+                    //       borderRadius: BorderRadius.circular(10),
+                    //     ),
+                    //   ),
+                    // ),
                     child: TextFormField(
+                      onTap: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1950),
+                          lastDate: DateTime(2050),
+                        );
+                        if (pickedDate != null) {
+                          fromDateControler.text =
+                              '${pickedDate.day}/${pickedDate.month}/${pickedDate.year}';
+                        }
+                      },
+                      readOnly: true,
+                      autocorrect: false,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      controller: fromDateControler,
                       decoration: InputDecoration(
-                        labelText: 'Due Date',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                        prefixIcon: const Icon(
+                          Icons.calendar_month_outlined,
+                          color: Colors.grey,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        hintText: 'Due Date',
+                        hintStyle: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        counterText: "",
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: formBorderColor, width: 1.0),
+                          borderRadius: BorderRadius.circular(20.0),
                         ),
                       ),
+                      keyboardType: TextInputType.datetime,
+                      textInputAction: TextInputAction.done,
+                      validator: (value) =>
+                          value!.isEmpty ? 'please give date' : null,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            if (value != null) {
+                              formBorderColor = const Color(0xFFE91e63);
+                            } else {
+                              formBorderColor = Colors.grey;
+                            }
+                          },
+                        );
+                      },
                     ),
                   ),
                 ),
                 Expanded(
                   child: Padding(
                     padding: const EdgeInsets.only(right: 20, left: 10),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Due Time',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          controller: _timeController,
+                          readOnly: true,
+                          onTap: () => _selectTime(context),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            labelText: 'Selected Time',
+                            prefixIcon: Icon(Icons.access_time),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
                   ),
                 ),
