46d2c290eea34b195ad9ba2897999651cb4cf0b0
Mukund K <koladiyamukund58@gmail.com>
2024-03-15 19:19:13 +0530

    Attendance Display

--- lib/student/attendance.dart
+++ lib/student/attendance.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
-class AttendanceDisplay extends StatelessWidget {
+import '../reusable_widget/reusable_textfield.dart';
+
+class AttendanceDisplay extends StatefulWidget {
   final String program;
   final String programTerm;
   final String division;
@@ -15,63 +20,140 @@ class AttendanceDisplay extends StatelessWidget {
   });
 
   @override
+  State<AttendanceDisplay> createState() => _AttendanceDisplayState();
+}
+
+class _AttendanceDisplayState extends State<AttendanceDisplay> {
+  final String _year = '2024';
+  final String _ay = '2023-24';
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Attendance Display'),
+        title: const Text('ATTENDANCE'),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.of(context).pop();
           },
         ),
       ),
-      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-        future: FirebaseFirestore.instance
-            .collection('students')
-            .doc(program)
-            .collection(programTerm)
-            .doc(division)
-            .collection('student')
-            .doc(userId)
-            .collection('monthlyAttendance')
-            .doc('March_2024') // Replace with the actual month and year
-            .get(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-
-          final data = snapshot.data?.data() ?? {};
-          final subjectAttendance =
-              data['subjectAttendance'] as Map<String, dynamic>? ?? {};
-
-          return ListView(
-             physics: AlwaysScrollableScrollPhysics(),
-            children: subjectAttendance.entries.map((entry) {
-              final subject = entry.key;
-              final attendanceData =
-                  entry.value as Map<String, dynamic>? ?? {};
-
-              final presentCount = attendanceData['presentCount'] as int? ?? 0;
-              final absentCount = attendanceData['absentCount'] as int? ?? 0;
-              final totalLectures = presentCount + absentCount;
-              final percentage = totalLectures != 0 ? ((presentCount / totalLectures) * 100).toStringAsFixed(1) : '0.0';
-
-              return Padding(
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              color: Color(0xff002233),
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child:Padding(
                 padding: const EdgeInsets.all(10),
-                child: ListTile(
-                  title: Text("${subject}"),
-                  trailing: Text("${percentage}"),
-                )
-              );
-            }).toList(),
-          );
-        },
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      const TextSpan(
+                        text: "Program: ",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: "${widget.program}\n"),
+                      const TextSpan(
+                        text: "Program Term: ",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: "${widget.programTerm}\n"),
+                      const TextSpan(
+                        text: "Division: ",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: "${widget.division}\n"),
+                      const TextSpan(
+                        text: "AY: ",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: "${_ay}"),
+                    ],
+                  ),
+                  style: const TextStyle(fontSize: 20,color: Colors.white),
+                ),
+              ),
+                )),
+          ),
+          Expanded(
+            child: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              future: FirebaseFirestore.instance
+                  .collection('students')
+                  .doc(widget.program)
+                  .collection(widget.programTerm)
+                  .doc(widget.division)
+                  .collection('student')
+                  .doc(widget.userId)
+                  .collection('yearlyAttendance')
+                  .doc(_year) // Replace with the actual month and year
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+                final data = snapshot.data?.data() ?? {};
+                final subjectAttendance =
+                    data['subjectAttendance'] as Map<String, dynamic>? ?? {};
+                if (subjectAttendance.isEmpty) {
+                  return const Center(
+                      child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                        'No Data Found ! Please Check Selected Month or Contact Admin '),
+                  ));
+                }
+                return ListView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  children: subjectAttendance.entries.map((entry) {
+                    final subject = entry.key;
+                    final attendanceData =
+                        entry.value as Map<String, dynamic>? ?? {};
+
+                    final presentCount =
+                        attendanceData['presentCount'] as int? ?? 0;
+                    final absentCount =
+                        attendanceData['absentCount'] as int? ?? 0;
+                    final totalLectures = presentCount + absentCount;
+                    final percentage = totalLectures != 0
+                        ? ((presentCount / totalLectures) * 100)
+                            .toStringAsFixed(1)
+                        : '0.0';
+
+                    return Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Card(
+                          child: ListTile(
+                            title: Text("${subject}"),
+                            trailing: Container(
+                                decoration: BoxDecoration(
+                                    color: const Color(0xff002233),
+                                    borderRadius: BorderRadius.circular(10)),
+                                height: 70,
+                                width: 90,
+                                child: Center(
+                                    child: Text(
+                                  "${percentage}%",
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ))),
+                          ),
+                        ));
+                  }).toList(),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
